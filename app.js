2e902004e215ebe2f0cd709883c37edf69391474
nallasaiteja <nallasaitejagupta@gmail.com>
2025-06-26 23:27:16 +0530

    setup autentication

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressErr = require("./utils/ExpressErr.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js")
@@ -53,11 +56,28 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
+
+app.get("/demouser", async (req,res)=>{
+  let fakeUser = new User({
+    email : "student@gmail.com",
+    username : "delta-student"
+  });
+
+  let registeredUser = await User.register(fakeUser,"helloworld");
+  res.send(registeredUser);
+})
 
 app.use("/listings",listings);
 app.use("/listings/:id/reviews",reviews);
